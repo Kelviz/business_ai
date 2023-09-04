f18fe2dcbb16d8789374dd2ea0762fda3ccbc451
Kelviz <mysviurch@gmail.com>
2023-09-04 09:47:33 +0100

    update activation url

--- business_ai/settings.py
+++ business_ai/settings.py
@@ -159,9 +159,9 @@ DJOSER = {
     'SEND_CONFIRMATION_EMAIL': True,
     'SET_USERNAME_RETYPE': True,
     'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': 'password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': 'email/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': 'activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': 'http://localhost:3000/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': 'http://localhost:3000/email/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': 'http://localhost:3000/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
 
     'SERIALIZERS': {
