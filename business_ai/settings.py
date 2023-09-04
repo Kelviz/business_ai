20c167aafc02f4af26fce827c6d677a9d4bebc47
Kelviz <70589535+Kelviz@users.noreply.github.com>
2023-09-04 10:44:24 +0100

    Update settings.py

--- business_ai/settings.py
+++ business_ai/settings.py
@@ -153,7 +153,7 @@ SIMPLE_JWT = {
 
 DJOSER = {
     'DOMAIN': 'http://localhost:3000',
-    'SITE_NAME': 'BizBurst'
+    'SITE_NAME': 'BizBurst',
     'LOGIN_FIELD': 'email',
     'USER_CREATE_PASSWORD_RETYPE': True,
     'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
@@ -161,9 +161,9 @@ DJOSER = {
     'SEND_CONFIRMATION_EMAIL': True,
     'SET_USERNAME_RETYPE': True,
     'SET_PASSWORD_RETYPE': True,
-    'PASSWORD_RESET_CONFIRM_URL': '/password/reset/confirm/{uid}/{token}',
-    'USERNAME_RESET_CONFIRM_URL': '/email/reset/confirm/{uid}/{token}',
-    'ACTIVATION_URL': '/activate/{uid}/{token}',
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/email/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
     'SEND_ACTIVATION_EMAIL': True,
 
     'SERIALIZERS': {
