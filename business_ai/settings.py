ce4aea3a092b3a501181966c43373f2c31afef4c
Kelviz <mysviurch@gmail.com>
2023-09-07 03:52:08 +0100

    modified cor headers

--- business_ai/settings.py
+++ business_ai/settings.py
@@ -188,6 +188,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
+CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
+CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
+
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
