e2c431fee6a2a40a50e6bbdacdb208a35fef7ad4
Kelviz <mysviurch@gmail.com>
2023-09-08 20:54:41 +0100

    update

--- business_ai/settings.py
+++ business_ai/settings.py
@@ -188,7 +188,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-CORS_ALLOW_HEADERS = ["Authorization", "Content-Type"]
+CORS_ALLOW_HEADERS = ["Authorization", "Content-Type", "x-user-id", ]
 CORS_ALLOW_METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
 
 
