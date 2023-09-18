e55b6c9b601df116f8367797ab69548461300946
Kelviz <mysviurch@gmail.com>
2023-09-18 12:48:36 +0100

    update

--- business_ai/settings.py
+++ business_ai/settings.py
@@ -212,8 +212,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 # Default primary key field type
